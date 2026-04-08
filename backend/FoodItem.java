ccec419b1a2e31a349043da1885bb2860b436d75
navanathmp <navanathmp6588@gmail.com>
2026-04-08 12:38:59 +0530

    Create FoodItem.java

---
+++ backend/FoodItem.java
@@ -0,0 +1,11 @@
+public class FoodItem {
+    int id;
+    String name;
+    double price;
+
+    public FoodItem(int id, String name, double price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
